74ee4cacdb061a658ce211a2a0a27d53960ab1cb
Ryoni <alfud8771@naver.com>
2023-05-19 16:51:45 +0900

    youtube 완성

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/youtube";
+import YoutubeClient from "../api/YoutubeClient";
 
 export const YoutubeApiContext = createContext();
 
-const youtube = new Youtube();
+const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
